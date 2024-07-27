e1149b6756f316f42593a0a1d4ffae247e349979
Renzusclarke <77496879+Renzusclarke@users.noreply.github.com>
2024-07-27 11:52:50 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# projet
+
+# Blue Bird Hotel
+
+This project is used in the context of a web development project within TEKUP, made By Aziz and Fares
+
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = "127.0.0.1";
+$username = "bluebird_user";
+$password = "password";
+$database = "bluebirdhotel";
+
+$conn = mysqli_connect($server,$username,$password,$database);
+
+if(!$conn){
+    die("<script>alert('connection Failed.')</script>");
+}
+// else{
+//     echo "<script>alert('connection successfully.')</script>";
+// }
+?>
